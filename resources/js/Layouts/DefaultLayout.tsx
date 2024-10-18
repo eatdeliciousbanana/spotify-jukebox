73d1bf2a9297fef3ae715307795613d4bcb409d6
eatdeliciousbanana <93957952+eatdeliciousbanana@users.noreply.github.com>
2024-10-18 12:53:57 +0900

    tailadmin

---
+++ resources/js/Layouts/DefaultLayout.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import { Head } from "@inertiajs/react";
+import Header from "@/Components/Header/index";
+import Sidebar from "@/Components/Sidebar/index";
+
+interface DefaultLayoutProps {
+    title?: string;
+    children: React.ReactNode;
+}
+
+const DefaultLayout = ({ title, children }: DefaultLayoutProps) => {
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    return (
+        <>
+            <Head title={title} />
+            <div className="dark:bg-boxdark-2 dark:text-bodydark">
+                <div className="flex h-screen overflow-hidden">
+                    <Sidebar
+                        sidebarOpen={sidebarOpen}
+                        setSidebarOpen={setSidebarOpen}
+                    />
+                    <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                        <Header
+                            sidebarOpen={sidebarOpen}
+                            setSidebarOpen={setSidebarOpen}
+                        />
+                        <main>
+                            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                                {children}
+                            </div>
+                        </main>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default DefaultLayout;
